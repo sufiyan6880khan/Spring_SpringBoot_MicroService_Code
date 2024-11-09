2c14709dbf49d99b4881178c52bcb405de4d87bf
sufiyan6880khan <sufiyan6880khan@gmail.com>
2024-11-09 15:45:34 +0530

    add project --Sufiyan

---
+++ BOOTIOCMiniproject11-DefaultProfile/src/main/java/com/sk/Service/EmployeeServiceImpl.java
@@ -0,0 +1,74 @@
+package com.sk.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.sk.DAO.IEmployeeDAO;
+import com.sk.Javabean.Employee;
+
+@Service("empService")
+public class EmployeeServiceImpl implements IEmployeeService
+{
+	
+  @Autowired
+	private IEmployeeDAO dao;
+	
+// SELECT OPERATION 
+	@Override
+	public List<Employee> fetchIEmployeeByDesgs(String desg1, String desg2, String desg3) throws Exception 
+	{
+
+		// use dao
+		List<Employee> list = dao.getEmployeeByDeg(desg1, desg2, desg3);
+	  
+	   
+		list.forEach((k)->
+		{
+		        Integer bSal = k.getBsal();
+				Float da = (float) (bSal * 0.2);
+				Float pf = (float)(bSal * 0.1);
+				Float totalSal = (float)(bSal + pf + da);
+			 
+				k.setDa(da); 
+				k.setPf(pf);
+		        k.setTotalSal(totalSal);
+		       
+		});
+
+		return list;
+	} // end of fetchEmployeee  method 
+	
+
+	
+	// UPDATE OPERATION
+	@Override
+	public String resgisterEmployee(Employee emp) throws Exception
+	{
+		  int result = dao.insertEmployee(emp);
+		  
+		  if (result == 1)
+		  {
+			  return "Employee resister successfully........";		 
+			  }
+
+		return "Employee not resister.......";
+	} // end of resisterEmployee method 
+
+	
+	
+	//  DELETE OPERATION
+	@Override
+	public String deleteEmployee(Employee emp) throws Exception {
+		
+		int result = dao.deleteEmployee(emp); 
+		
+		if(result == 1) {
+			return "Employee details delete successfully";
+		}
+		return "Employee details not available please give valid employee no..";
+	}  /// end of deleteEMPLOYEE METHOD
+
+} // class
