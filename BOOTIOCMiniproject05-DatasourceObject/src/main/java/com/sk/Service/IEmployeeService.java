2c425fb0e97cd81abe935298d85c50dd843cc988
sufiyan6880khan <sufiyan6880khan@gmail.com>
2024-11-09 15:43:19 +0530

    add project --Sufiyan

---
+++ BOOTIOCMiniproject05-DatasourceObject/src/main/java/com/sk/Service/IEmployeeService.java
@@ -0,0 +1,15 @@
+package com.sk.Service;
+import java.util.List;
+
+import com.sk.Javabean.Employee;
+
+public interface IEmployeeService 
+{
+
+	public List<Employee> fetchIEmployeeByDesgs(String desg1, String desg2,String desg3) throws Exception ;
+	
+	public String resgisterEmployee(Employee emp) throws Exception;
+	
+	public String deleteEmployee(Employee emp) throws Exception;
+	
+}
