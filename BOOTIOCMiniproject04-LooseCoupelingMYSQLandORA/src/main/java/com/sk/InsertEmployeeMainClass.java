2c57ae05578c7136eda86f72a1f74da36a2fd9d5
sufiyan6880khan <sufiyan6880khan@gmail.com>
2024-11-09 15:42:48 +0530

    add project --Sufiyan

---
+++ BOOTIOCMiniproject04-LooseCoupelingMYSQLandORA/src/main/java/com/sk/InsertEmployeeMainClass.java
@@ -0,0 +1,65 @@
+package com.sk;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+ 
+import com.sk.Controller.OperationController;
+import com.sk.Javabean.Employee;
+
+@SpringBootApplication
+public class InsertEmployeeMainClass 
+{
+ 
+	public static void main(String[] args) 
+	{
+		// get IOC container
+	ApplicationContext  ctx = SpringApplication.run(InsertEmployeeMainClass.class, args); 
+	
+	// get Controller class Object reference 
+	OperationController controller = ctx.getBean("oController",OperationController.class);
+	
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the Employe- Name");
+	String ename = s.nextLine();
+	System.out.println("Enter the Employe- JOB");
+	String job = s.nextLine();
+	System.out.println("Enter the Employe- Manager id must be (7566, 7698, 7782, 7839,7902,7788)");
+	int mgr =s.nextInt(); 
+	System.out.println("Enter the Employe- SALARY");
+	int sal =s.nextInt(); 
+	System.out.println("Enter the Employe- Commision");
+	int comm =s.nextInt(); 
+	System.out.println("Enter the Employe- DEPTNO..(10..20..30..40...)");
+	int deptno = s.nextInt();	
+	 s.close();
+       try{
+    	    
+    	   Employee emp =new Employee();
+    	   emp.setEname(ename);
+    	   emp.setJob(job);
+    	   emp.setBsal(sal);
+    	   emp.setDeptno(deptno);
+    	   emp.setComm(comm);
+    	   emp.setMgr(mgr);
+    	   
+    	   
+    	   String msg = controller.processEmployee(emp);
+    	   System.out.println(msg);
+       }
+      
+    	    
+       catch (Exception e) 
+       {
+    	   e.printStackTrace();
+	        
+	  } // end of catch
+		
+		// close the container
+       ((ConfigurableApplicationContext)ctx).close();
+		
+	}
+}
+   
